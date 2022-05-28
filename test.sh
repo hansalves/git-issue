be6162406ee92a87aac09bdb444dd3a2e954cd07
Hans Alves <halves-git@qzwx.nl>
2022-05-28 15:51:29 +0200

    use tput to determine and set terminal colors

--- test.sh
+++ test.sh
@@ -43,9 +43,14 @@ message()
   sed "s/$gi_re/gi/"
 }
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
+RED=''
+GREEN=''
+NC=''
+if [ -t 1 ] && command -v tput &> /dev/null && [ `tput colors` -gt 2 ] ; then
+  RED=`tput setaf 1`
+  GREEN=`tput setaf 2`
+  NC=`tput sgr0`
+fi
 
 ok()
 {
